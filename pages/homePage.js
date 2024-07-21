367e162c4b35388e69f71461fea83502db33129e
Diana Lipanova <altere2804@gmail.com>
2024-07-21 12:16:46 +0200

    feat: add the home page structure

---
+++ pages/homePage.js
@@ -0,0 +1,12 @@
+export function loadHome(app) {
+    app.innerHTML = `
+   
+    <p>Пройдите точный и быстрый </p>
+    <h1 class="testTitle">Тест на определение IQ </h1>
+    <img src="../assets/images/brain.png" alt="brain image">
+    <button>Пройти тест</button>
+    <p> И получите рекомендации 
+по развитию своего интеллекта </p>
+    <p>и улучшению финансового благосостояния и личной жизни</p>
+  `;
+}
