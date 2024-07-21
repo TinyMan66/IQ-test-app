99f843a977b195cd4e764619accf0e11960f5b41
Diana Lipanova <altere2804@gmail.com>
2024-07-21 18:11:10 +0200

    feat: home page elements added

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,12 +1,13 @@
 export function loadHome(app) {
     app.innerHTML = `
    
-    <p>Пройдите точный и быстрый </p>
-    <h1 class="testTitle">Тест на определение IQ </h1>
-    <img src="../assets/images/brain.png" alt="brain image">
-    <button>Пройти тест</button>
-    <p> И получите рекомендации 
-по развитию своего интеллекта </p>
-    <p>и улучшению финансового благосостояния и личной жизни</p>
+    <div class="main">
+    <span class="description">Пройдите точный и быстрый </span>
+    <h1 class="test-title">Тест на определение IQ </h1>
+    <img src="../assets/images/main_brain.png" alt="brain picture">
+    <button class="btn">Пройти тест</button>
+    <span class="yellow-text"> И получите рекомендации по развитию своего интеллекта </span>
+    <span class="white-text">и улучшению финансового благосостояния и личной жизни</span>
+    </div>
   `;
 }
