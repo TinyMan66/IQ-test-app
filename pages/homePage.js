969832e5470df11417f377fd724b6d7e53c13a4e
Diana Lipanova <altere2804@gmail.com>
2024-07-22 20:10:49 +0200

    feat: more info tags added

--- pages/homePage.js
+++ pages/homePage.js
@@ -5,9 +5,13 @@ export function loadHome(app) {
     <span class="description">Пройдите точный и быстрый </span>
     <h1 class="test-title">Тест на определение IQ </h1>
     <img src="../assets/images/main_brain.png" alt="brain picture">
-    <button class="btn">Пройти тест</button>
+    <button class="btn test-btn">Пройти тест</button>
     <span class="yellow-text"> И получите рекомендации по развитию своего интеллекта </span>
     <span class="white-text">и улучшению финансового благосостояния и личной жизни</span>
+    <div class="more-info">
+    <button class=" btn more-info-btn"><img src="../assets/icons/arrowUp.svg" alt="arrow up"></button>
+    <span>Подробнее</span>
+    </div>
     </div>
   `;
 }
