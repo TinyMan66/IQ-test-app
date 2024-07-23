6ca5666fddffefa10fb866f47093713391b23669
Diana Lipanova <altere2804@gmail.com>
2024-07-23 14:39:14 +0200

    feat: description block added to the testPage

--- pages/testPage.js
+++ pages/testPage.js
@@ -1,5 +1,10 @@
-export const TestPage = (container) => {
-    container.innerHTML = `
-        <h1>Tест</h1>
-    `;
-};
+export function loadTest(app) {
+    app.innerHTML = `
+    <div class="test-page">
+        <p class="test-info">
+        Прохождение теста займет у вас не более<span>12 минут</span>, а его результаты вы сможете<span>использовать всю жизнь. </span>
+        </p>
+    </div>
+  `;
+}
+
