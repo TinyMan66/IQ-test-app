daf5f517526ccf9d34e8b67fd4276babe3bd01d0
Diana Lipanova <altere2804@gmail.com>
2024-07-23 13:40:19 +0200

    feat: the p tag added to the aboutPage

---
+++ pages/aboutPage.js
@@ -0,0 +1,7 @@
+export function loadAbout(app) {
+    app.innerHTML = `
+    <p>Также по результатам теста <strong>вы получите</strong> подробные <strong>советы</strong> по определению наиболее перспективной <strong>для
+        вашего типа</strong> <strong class="bold">интеллекта</strong> <strong>сферы деятельности</strong>, которая принесет вам скорейший финансовый результат.
+    </p>
+  `;
+}
