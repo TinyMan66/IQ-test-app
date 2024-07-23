3933c398b7128fd6c0a54c92137e0c73259122b8
Diana Lipanova <altere2804@gmail.com>
2024-07-23 13:56:21 +0200

    feat: brain image added to the aboutPage

--- pages/aboutPage.js
+++ pages/aboutPage.js
@@ -1,7 +1,10 @@
 export function loadAbout(app) {
     app.innerHTML = `
-    <p>Также по результатам теста <strong>вы получите</strong> подробные <strong>советы</strong> по определению наиболее перспективной <strong>для
+    <div class="about-page">
+        <p class="test-description">Также по результатам теста <strong>вы получите</strong> подробные <strong>советы</strong> по определению наиболее перспективной <strong>для
         вашего типа</strong> <strong class="bold">интеллекта</strong> <strong>сферы деятельности</strong>, которая принесет вам скорейший финансовый результат.
-    </p>
+        </p>
+        <img src="../assets/images/brain_2.png" alt="brain picture">
+    </div>
   `;
 }
