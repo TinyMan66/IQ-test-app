a9bf79bbbb584b9c08f907565cc14b18d205f2d7
Diana Lipanova <altere2804@gmail.com>
2024-07-25 15:27:05 +0200

    feat: start test function added to the test page

--- pages/testPage.js
+++ pages/testPage.js
@@ -10,5 +10,47 @@ export function loadTest(app) {
         <button class="btn test-info-btn">Пройти тест</button>
     </div>
   `;
+    document.querySelector('.test-info-btn').addEventListener('click', startTest);
 }
 
+function startTest() {
+    const app = document.getElementById('app');
+    app.innerHTML = `
+        <nav class="test-nav">
+            <div class="menu-toggle" id="menu-toggle">
+                <img src="../assets/icons/navBar.svg" alt="menu icon">
+            </div>
+            <div class="menu" id="menu">
+                <div class="button-container">
+                    <button class="close-btn">
+                        <img src="../assets/icons/close.svg" alt="close icon">
+                    </button>
+                </div>
+                <a href="#" data-page="home">Главная</a>
+                <a href="#" data-page="about">Информация о тесте</a>
+                <a href="#" data-page="test">Пройти тест</a>
+            </div>
+            <img src="../assets/images/main_brain.png" alt="brain picture" class="test-nav-image">
+            <span class="nav-text">Тест на определение IQ</span>
+        </nav>
+        <div class="test-container">
+            <div class="progress-bar-container">
+                <div class="progress-bar"></div>
+            </div>
+            <div class="test-question"></div>
+            <div class="test-answers"></div>
+            <button class="btn next-btn">Далее</button>
+        </div>
+    `;
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+    const closeButton = document.querySelector('.close-btn');
+
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('open');
+    });
+
+    closeButton.addEventListener('click', function() {
+        menu.classList.remove('open');
+    });
+}
